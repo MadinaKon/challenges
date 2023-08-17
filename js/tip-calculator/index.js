43351a6f3a1fb22860a8a124a6f25aab2b3c495e
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2023-08-17 15:17:13 +0200

    add variables and do calculations

---
+++ js/tip-calculator/index.js
@@ -0,0 +1,6 @@
+const mealCost = 6.9;
+const tipPercentage = 20;
+const tipAmount = (mealCost * tipPercentage) / 100;
+const totalCost = mealCost + tipAmount;
+
+console.log("The total cost of your bill is: ", totalCost);
