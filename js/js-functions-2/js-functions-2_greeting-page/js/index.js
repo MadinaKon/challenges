11081464b06fb166c36a3134ffb0262e04126603
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2023-08-21 15:21:48 +0200

    add Change the contents of this page depending on the current day and time

--- js/js-functions-2/js-functions-2_greeting-page/js/index.js
+++ js/js-functions-2/js-functions-2_greeting-page/js/index.js
@@ -22,8 +22,20 @@ Change the contents of this page depending on the current day and time.
 
 const display = document.querySelector('[data-js="display"]');
 
+const currentTime = new Date().getHours();
+console.log("currentTime ", currentTime);
+
 function getGreeting() {
   // Code here
+  if (currentTime >= 6 && currentTime <= 12) {
+    return "Good Morning";
+  } else if (currentTime >= 13 && currentTime <= 18) {
+    return "Good Afternoon";
+  } else if (currentTime >= 19 && currentTime <= 22) {
+    return "Good Evening";
+  } else if (currentTime >= 23 && currentTime <= 5) {
+    return "Good Night";
+  }
 }
 
 function getDayColor() {
