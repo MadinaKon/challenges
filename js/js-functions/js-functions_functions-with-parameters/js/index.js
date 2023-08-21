157cb139a7adb474fa882e0d4bdc58b1d6127912
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2023-08-21 10:55:17 +0200

    add Functions with parameters

--- js/js-functions/js-functions_functions-with-parameters/js/index.js
+++ js/js-functions/js-functions_functions-with-parameters/js/index.js
@@ -7,7 +7,8 @@ console.log("Exercise 1:");
 */
 
 // Uncomment the next line of code and figure out why it does not work:
-// logText;
+// Solution was to add the missing parentheses, when invoking the function logText
+logText();
 
 function logText() {
   console.log("This text does not appear. Why?");
@@ -19,34 +20,38 @@ console.log("Exercise 2:");
 2: Look at the functions below. They are almost identical. Can you find a way to generalize them into a new function with an input parameter? Replace the function calls below with your new function.
 */
 
-function greetBob() {
-  console.log("Welcome Bob, good to see you again!");
-}
-
-function greetAlice() {
-  console.log("Welcome Alice, good to see you again!");
-}
-
-function greetMary() {
+function greet(name) {
   console.log("Welcome Mary, good to see you again!");
 }
 
-greetAlice();
-greetBob();
-greetMary();
+/*
+2: Look at the functions below. They are almost identical. Can you find a way to generalize them into a new function with an input parameter? Replace the function calls below with your new function.
+*/
 
 // --v-- write your code here --v--
 
-function greet() {}
+function greet(name) {
+  console.log(`Welcome ${name}, good to see you again!`);
+}
 
+greet("Bob");
+greet("Alice");
+greet("Mary");
 // --^-- write your code here --^--
 
 console.log("-------------");
 console.log("3:");
 /*
-3: We log a little separation text for each subtask. This is repetitive code, so lets write a function 'logSeparator' which takes the number of the exercise as an input parameter and logs the respective separator. then use this function to replace the existing separator logs in this javascript file.
+3: We log a little separation text for each subtask. This is repetitive code, so lets write a function 'logSeparator' which takes the number of the exercise as an input parameter
+ and logs the respective separator. then use this function to replace the existing separator logs in this javascript file.
 */
 
 // --v-- write your code here --v--
+function logSeparator(numberOfExercise, separator) {
+  console.log(numberOfExercise, separator);
+}
 
+logSeparator("Exercise 1:", "-------------");
+logSeparator("Exercise 2:", "-------------");
+logSeparator("3:", "-------------");
 // --^-- write your code here --^--
