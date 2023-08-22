ce21cac12bedc1f2f10f9369ce7e95659f3d7360
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2023-08-22 15:32:26 +0200

    without switch button case

--- js/js-inputs-and-strings/js-inputs-and-strings_switch-values/js/index.js
+++ js/js-inputs-and-strings/js-inputs-and-strings_switch-values/js/index.js
@@ -14,8 +14,10 @@ Hint: the HTML entity for the switch button is: &updownarrow
 */
 
 const firstInput = document.querySelector('[data-js="first-input"]');
+const secondInput = document.querySelector('[data-js="second-input"]');
 const uppercaseButton = document.querySelector('[data-js="button-uppercase"]');
 const lowercaseButton = document.querySelector('[data-js="button-lowercase"]');
+const switchButton = document.querySelector('[data-js="button-switch"]');
 
 uppercaseButton.addEventListener("click", () => {
   firstInput.value = firstInput.value.toUpperCase();
@@ -23,4 +25,16 @@ uppercaseButton.addEventListener("click", () => {
 
 lowercaseButton.addEventListener("click", () => {
   firstInput.value = firstInput.value.toLowerCase();
+});
+
+// uppercaseButton.addEventListener("click", () => {
+//   secondInput.value = secondInput.value.toUpperCase();
+// });
+
+// lowercaseButton.addEventListener("click", () => {
+//   secondInput.value = secondInput.value.toLowerCase();
+// });
+
+switchButton.addEventListener("click", (event) => {
+  console.log("switchButton is clicked", event);
 });
