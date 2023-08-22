7c5fbc9ebcbfaf91ea4300c090840bdff4698d99
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2023-08-22 16:29:16 +0200

    multiple-inputs done

--- js/js-inputs-and-strings/js-inputs-and-strings_multiple-inputs/js/index.js
+++ js/js-inputs-and-strings/js-inputs-and-strings_multiple-inputs/js/index.js
@@ -14,3 +14,34 @@ Implement the following functionality:
 const firstInput = document.querySelector("[data-js=first-input]");
 const secondInput = document.querySelector("[data-js=second-input]");
 const button = document.querySelector("[data-js=button]");
+
+// 1. On button click: The value of the first input field is copied into the second input field
+// button.addEventListener("click", () => {
+//   let copyFirst = firstInput.value;
+
+//   secondInput.value = copyFirst;
+
+//   console.log("firstInput.value ", firstInput.value);
+//   console.log("secondInput.value ", secondInput.value);
+//   console.log("copyFirst ", copyFirst);
+// });
+
+// 2. On button click: The value of the first input field is copied in uppercase into the second input field
+// button.addEventListener("click", () => {
+//   const oldValue = firstInput.value;
+//   const newValue = oldValue.toUpperCase();
+
+//   secondInput.value = newValue;
+
+//   console.log("firstInput.value ", firstInput.value);
+//   console.log("newValue ", newValue);
+//   console.log("secondInput.value ", secondInput.value);
+// });
+
+// 3. On button click: The values of the two input fields switch.
+button.addEventListener("click", () => {
+  let extraCup = firstInput.value;
+
+  firstInput.value = secondInput.value;
+  secondInput.value = extraCup;
+});
