59b7273f1beed29aac8a1236e50a820f80e74497
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2023-08-23 11:00:45 +0200

    Log all form data (in object form)

--- js/js-forms/js-forms_handle-form-submit/js/index.js
+++ js/js-forms/js-forms_handle-form-submit/js/index.js
@@ -1,3 +1,15 @@
 console.clear();
 
 const form = document.querySelector('[data-js="form"]');
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const formData = new FormData(event.target);
+  console.log("formData ", formData);
+
+  const data = Object.fromEntries(formData);
+  let ageBadnessSum = data.age + data.badness;
+
+  console.log(`The age-badness-sum of ${data.firstName} is ${ageBadnessSum}`);
+});
