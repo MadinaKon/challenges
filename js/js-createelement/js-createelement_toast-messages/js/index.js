6d1583bd0cc295f3485354d39f43999be2cb12fb
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2023-08-23 16:43:44 +0200

    add Clear all toast messages with .innerHTML

--- js/js-createelement/js-createelement_toast-messages/js/index.js
+++ js/js-createelement/js-createelement_toast-messages/js/index.js
@@ -6,8 +6,15 @@ const clearButton = document.querySelector('[data-js="clear-button"]');
 
 addButton.addEventListener("click", () => {
   // Exercise: Append a new entry to the toast messages container
+  const newToastMessage = document.createElement("li");
+
+  newToastMessage.classList.add("toast-container__message");
+  newToastMessage.textContent = "I'm an added toast message";
+
+  toastContainer.append(newToastMessage);
 });
 
 clearButton.addEventListener("click", () => {
   // Exercise: Clear the stack of toast messages
+  toastContainer.innerHTML = "";
 });
