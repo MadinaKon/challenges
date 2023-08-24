44fd4cb155fe488c61a564ee641be6947432e1a0
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2023-08-24 12:56:00 +0200

    extracted to functions for header and paragraph

--- js/js-forms-2/js-forms-2_simple-todolist/js/index.js
+++ js/js-forms-2/js-forms-2_simple-todolist/js/index.js
@@ -7,10 +7,8 @@ const headline = document.getElementById("headline");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   const formData = new FormData(e.target);
-  console.log("formData ", formData);
 
   const data = Object.fromEntries(formData);
-  console.log("DATA ", data);
 
   createListEntries(data.headline, data.task);
 
@@ -20,17 +18,25 @@ form.addEventListener("submit", (e) => {
 
 function createListEntries(headline, task) {
   const li = document.createElement("li");
-  li.className = "todoList--list--item";
 
-  const headlineAppended = document.createElement("h2");
-  headlineAppended.className = "todoList--list--title";
-  headlineAppended.textContent = headline;
-  li.append(headlineAppended);
+  const headerElement = createHeadline(headline);
+  li.append(headerElement);
 
-  const taskText = document.createElement("p");
-  taskText.textContent = task;
-
-  li.append(taskText);
+  const pElement = createParagraph(task);
+  li.append(pElement);
 
   todoList.append(li);
+}
+
+function createHeadline(text) {
+  const headlineAppended = document.createElement("h2");
+  headlineAppended.className = "todoList--list--title";
+  headlineAppended.textContent = text;
+  return headlineAppended;
+}
+
+function createParagraph(paragraphText) {
+  const taskText = document.createElement("p");
+  taskText.textContent = paragraphText;
+  return taskText;
 }
