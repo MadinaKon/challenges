5ce61b40147b226e92d9fdaee3b177514480e8bd
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2023-08-24 16:14:32 +0200

    changed string interpolation to template literals

--- js/js-createelement/js-createelement_product-teaser/js/index.js
+++ js/js-createelement/js-createelement_product-teaser/js/index.js
@@ -12,43 +12,32 @@ const price = "149,99 €";
 const imageSrc =
   "https://unsplash.com/photos/3VOTHTrE614/download?ixid=MnwxMjA3fDB8MXxhbGx8fHx8fHx8fHwxNjU5NTM3NTA2&force=true&w=640";
 
-productArticle.innerHTML +=
-  '<div class="product__body">' +
-  '<div class="product__text-container">' +
-  '<h2 class="product__name">' +
-  name +
-  "</h2>" +
-  '<ul class="product__categories">' +
-  '<li class="product__category">' +
-  category1 +
-  "</li>" +
-  '<li class="product__category">' +
-  category2 +
-  "</li>" +
-  '<li class="product__category">' +
-  category3 +
-  "</li>" +
-  "</ul>" +
-  '<p class="product__description">' +
-  description +
-  "</p>" +
-  "</div>" +
-  '<div class="product__image-container">' +
-  '<img class="product__image"' +
-  "src=" +
-  imageSrc +
-  ' alt="">' +
-  "</div>" +
-  "</div>" +
-  '<footer class="product__footer">' +
-  '<span class="product__price">' +
-  price +
-  "</span>" +
-  '<button type="button" class="product__buy-button-2">Buy</button>' +
-  "</footer>";
+productArticle.innerHTML += `
+<article class="product">
+      <div class="product__body">
+        <div class="product__text-container">
+          <h2 class="product__name">${name}</h2>
+          <ul class="product__categories">
+            <li class="product__category">${category1}</li>
+            <li class="product__category">${category2}</li>
+            <li class="product__category">${category3}</li>
+          </ul>
+          <p class="product__description">
+           ${description}
+          </p>
+        </div>
+        <div class="product__image-container">
+          <img class="product__image" src=${imageSrc} alt="">
+        </div>
+      </div>
+      <footer class="product__footer">
+        <span class="product__price">${price}</span>
+        <button type="button" class="product__buy-button-2">Buy</button>
+      </footer>
+</article>
+`;
 
 const buttonTwo = document.querySelector(".product__buy-button-2");
-console.log("buttonTwo ", buttonTwo);
 
 buttonTwo.addEventListener("click", () => {
   console.log(name, price);
