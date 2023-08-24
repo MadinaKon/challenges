8b922342f453fe4b6ff87f97f1292f6bc4b9ccc5
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2023-08-24 11:22:13 +0200

    reassigning the value1 and value2  with the user input for firstInput and secondInput fields

--- js/js-forms-2/js-forms-2_simple-live-calculator/js/index.js
+++ js/js-forms-2/js-forms-2_simple-live-calculator/js/index.js
@@ -3,3 +3,22 @@ console.clear();
 const firstInput = document.querySelector('[data-js="firstValue"]');
 const secondInput = document.querySelector('[data-js="secondValue"]');
 const result = document.querySelector('[data-js="result"]');
+
+let value1 = firstInput.value;
+let value2 = secondInput.value;
+
+firstInput.addEventListener("input", () => {
+  // reassigning the value1 with the user input for firstInput
+  value1 = firstInput.value;
+  multiply(value1, value2);
+});
+
+secondInput.addEventListener("input", () => {
+  // reassigning the value2 with the user input for secondInput
+  value2 = secondInput.value;
+  multiply(value1, value2);
+});
+
+function multiply(a, b) {
+  result.textContent = a * b;
+}
