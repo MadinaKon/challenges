e27d02d9331fce1252cb70bb5b92e31b4797b610
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2023-08-29 06:31:07 +0200

    rendering 5 empty stars

--- js/js-loops/js-loops_stars/js/index.js
+++ js/js-loops/js-loops_stars/js/index.js
@@ -7,7 +7,12 @@ function renderStars() {
   starContainer.innerHTML = "";
 
   //--v-- your code here --v--
+  for (let i = 1; i < 5; i++) {
+    let image = document.createElement("img");
+    image.src = "./assets/star-empty.svg";
 
+    starContainer.append(image);
+  }
   //--^-- your code here --^--
 }
 
