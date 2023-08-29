f16b1ca3be98f7adbdd91213d15506da203f9b54
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2023-08-29 06:45:09 +0200

    render a certain number of filled stars, depending on the argument given to the function

--- js/js-loops/js-loops_stars/js/index.js
+++ js/js-loops/js-loops_stars/js/index.js
@@ -2,18 +2,22 @@ console.clear();
 
 const starContainer = document.querySelector('[data-js="star-container"]');
 
-function renderStars() {
+function renderStars(filledStars = 0) {
   // reset the star container before rerendering stars
   starContainer.innerHTML = "";
 
   //--v-- your code here --v--
-  for (let i = 1; i < 5; i++) {
+  for (let i = 1; i <= 5; i++) {
     let image = document.createElement("img");
     image.src = "./assets/star-empty.svg";
+
+    if (i <= filledStars) {
+      image.src = "./assets/star-filled.svg";
+    }
 
     starContainer.append(image);
   }
   //--^-- your code here --^--
 }
 
-renderStars();
+renderStars(2);
