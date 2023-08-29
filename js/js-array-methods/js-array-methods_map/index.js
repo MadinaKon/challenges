20c1eef78d1c114fd3da02bf9cc06b24d135a1a1
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2023-08-29 15:52:19 +0200

    return object with keys question and answer

--- js/js-array-methods/js-array-methods_map/index.js
+++ js/js-array-methods/js-array-methods_map/index.js
@@ -24,10 +24,25 @@ const cards = [
   },
 ];
 
-const lowerCaseAnswers = null; // ['as often as you like.', ...]
+const lowerCaseAnswers = cards.map((card) => {
+  return card.answer.toLowerCase();
+});
 
-const questionsAndAnswersTogether = null; // ["How often can I use <header>? - As often as you like.", ...]
+// ["How often can I use <header>? - As often as you like.", ...]
+const questionsAndAnswersTogether = cards.map((card) => {
+  return card.question + " -" + card.answer;
+});
 
-const questionAndAnswer = null; // [{ question: 'How often can I use <header>?', answer: 'As often as you like.'}, {...}]
+// [{ question: 'How often can I use <header>?', answer: 'As often as you like.'}, {...}]
+const questionAndAnswer = cards.map((card) => {
+  return {
+    question: card.question,
+    answer: card.answer,
+  };
+});
+
+// console.log("lowerCaseAnswers ", lowerCaseAnswers);
+// console.log("questionsAndAnswersTogether ", questionsAndAnswersTogether);
+console.log("questionAndAnswer ", questionAndAnswer);
 
 export { lowerCaseAnswers, questionsAndAnswersTogether, questionAndAnswer };
