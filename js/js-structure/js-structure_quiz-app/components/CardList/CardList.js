9155f9f0cb6b317b1b95c3f0f7cdd3a5b79e563b
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2023-08-30 16:11:59 +0200

    fix cards is not defined errors

---
+++ js/js-structure/js-structure_quiz-app/components/CardList/CardList.js
@@ -0,0 +1,14 @@
+import Card from "../Card/Card.js";
+import { cards } from "../../utils/cards.js";
+
+export default function CardList() {
+  const cardList = document.createElement("section");
+  cardList.classList.add("card-list");
+
+  cards.forEach((card) => {
+    const cardElement = Card(card);
+    cardList.append(cardElement);
+  });
+
+  return cardList;
+}
