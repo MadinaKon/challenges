a94740353a8e761b9a94828169c0261177ea34a3
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2023-08-31 15:52:17 +0200

    solved filteredNews

--- js/js-array-methods-2/js-array-methods-2_news-flash/index.js
+++ js/js-array-methods-2/js-array-methods-2_news-flash/index.js
@@ -4,10 +4,20 @@ import { checkFilteredNews, checkSortedNews } from "./utils/results.js";
 
 const container = document.querySelector('[data-js="card-container"]');
 
+// Have a look at the js/index.js file: There is a variable filteredNews declared with news.filter() and a return value true.
+
+// You task is to change the filter method:
+
+// filter all cards which have the category "politics" in their category array
+// to do so, use the includes method on card.categories
+// don't forget to remove the return statement because you don't need it.
+
 // Part 1 - start here
-const filteredNews = news.filter(() => {
-  return true;
-});
+const filteredNews = news.filter((card) =>
+  card.categories.includes("politics")
+);
+
+console.log("filteredNews ", filteredNews);
 
 // Part 2 - start here
 const sortedNews = filteredNews;
