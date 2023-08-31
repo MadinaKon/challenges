3b0ad673b3c1c704a1124a21786a8401faae44d0
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2023-08-31 13:48:27 +0200

    solving

--- js/js-array-methods-2/js-array-methods-2_animal-objects/index.js
+++ js/js-array-methods-2/js-array-methods-2_animal-objects/index.js
@@ -78,9 +78,13 @@ const animals = [
 ];
 
 // Hint: Besides the array method, check out the string method `startsWith()`.
-const firstAnimalStartingWithLetterG = null;
+const firstAnimalStartingWithLetterG = animals.find((animal) =>
+  animal.name.startsWith("g")
+);
 
-const indexOfAnimalWithNameLongerFive = null;
+const indexOfAnimalWithNameLongerFive = animals.findIndex(
+  (animal) => animal.name.length >= 5
+);
 
 // Note:
 // - Sorting strings is slightly more complicated than sorting numbers.
@@ -91,16 +95,30 @@ const indexOfAnimalWithNameLongerFive = null;
 // Hint: sort() mutates the original array, which is bad.
 // -> Use animals.slice().sort(...) to make a copy (and the tests work).
 
-const animalsSortedAlphabetically = null;
+const animalsSortedAlphabetically = animals.slice().sort((a, b) => {
+  if (a.name < b.name) {
+    return -1;
+  }
+  if (a.name > b.name) {
+    return 1;
+  }
+  return 0;
+});
 
-const animalsSortedByWeightStartingWithLowest = null;
+const animalsSortedByWeightStartingWithLowest = animals.slice().sort((a, b) => {
+  return a.weight - b.weight;
+});
 
-const animalsSortedByWeightReversed = null;
+const animalsSortedByWeightReversed = animals.slice().sort((a, b) => {
+  return b.weight - a.weight;
+});
 
-const animalWithWeightMoreThanFivehundredExists = null;
+const weightMoreThan500 = (animal) => animal.weight > 500;
+const animalWithWeightMoreThanFivehundredExists =
+  animals.some(weightMoreThan500);
 
 // Hint: Filter for Europe first, then check every animal for its weight.
-const allAnimalsInEuropeWeighLessThanOnehundred = null;
+const allAnimalsInEuropeWeighLessThanOnehundred = animals.filter()
 
 // Hint: filter + map + reduce
 const weightOfAllAnimalsInAfrica = null;
