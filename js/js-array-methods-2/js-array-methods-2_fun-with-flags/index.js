2d1eab0d1fd7439e904e39f30eea7ef5de26befc
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2023-08-31 16:36:04 +0200

    Find One Matching Country implement

--- js/js-array-methods-2/js-array-methods-2_fun-with-flags/index.js
+++ js/js-array-methods-2/js-array-methods-2_fun-with-flags/index.js
@@ -4,12 +4,20 @@ import { Country } from "./components/Country/Country.js";
 const container = document.querySelector('[data-js="card-container"]');
 const queryInput = document.querySelector('[data-js="query-input"]');
 
+// 1. Find One Matching Country
+// Inside the event listener, use the find method to find the country in the countries array
+// which name starts with the search string.
+// 💡 Strings have a method startsWith(string), which might be helpful.
+// 💡 Each contry in the array as a name attribute.
+
 queryInput.addEventListener("input", (event) => {
   container.innerHTML = "";
 
   const searchString = event.target.value;
 
-  const foundCountry = null;
+  const foundCountry = countries.find((country) =>
+    country.name.startsWith(searchString)
+  );
 
   if (foundCountry) {
     const countryElement = Country(foundCountry);
