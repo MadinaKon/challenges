33e5c4c2db66310754c9a95c26eafdd46cee0e3d
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2023-08-31 16:37:01 +0200

    remove comments

--- js/js-array-methods-2/js-array-methods-2_fun-with-flags/index.js
+++ js/js-array-methods-2/js-array-methods-2_fun-with-flags/index.js
@@ -4,11 +4,6 @@ import { Country } from "./components/Country/Country.js";
 const container = document.querySelector('[data-js="card-container"]');
 const queryInput = document.querySelector('[data-js="query-input"]');
 
-// 1. Find One Matching Country
-// Inside the event listener, use the find method to find the country in the countries array
-// which name starts with the search string.
-// 💡 Strings have a method startsWith(string), which might be helpful.
-// 💡 Each contry in the array as a name attribute.
 
 queryInput.addEventListener("input", (event) => {
   container.innerHTML = "";
