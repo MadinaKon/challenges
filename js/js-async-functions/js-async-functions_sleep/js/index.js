aa84335e0f17f55259aec199a9c0b57b17309e45
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2023-09-02 11:45:59 +0200

    added dark mode

--- js/js-async-functions/js-async-functions_sleep/js/index.js
+++ js/js-async-functions/js-async-functions_sleep/js/index.js
@@ -7,20 +7,26 @@ const body = document.body;
 // --v-- code here --v--
 button3.addEventListener("click", async () => {
   icon.textContent = "💤";
+  body.classList.add("dark");
   await sleep(3);
   icon.textContent = "⏰";
+  body.classList.remove("dark");
 });
 
 button5.addEventListener("click", async () => {
   icon.textContent = "💤";
+  body.classList.add("dark");
   await sleep(5);
   icon.textContent = "⏰";
+  body.classList.remove("dark");
 });
 
 async function handleSleepFor10Button() {
   icon.textContent = "💤";
+  body.classList.add("dark");
   await sleep(10);
   icon.textContent = "⏰";
+  body.classList.remove("dark");
 }
 
 button10.addEventListener("click", handleSleepFor10Button);
