67380f8cdf56a57dcf98b5cfc0d4655404c0d7df
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2023-09-04 11:08:10 +0200

    use fetch with async&await

--- js/js-fetch/js-fetch_star-wars-console/js/index.js
+++ js/js-fetch/js-fetch_star-wars-console/js/index.js
@@ -2,6 +2,21 @@ console.clear();
 
 const url = "https://swapi.dev/api/people";
 
-function fetchData() {}
+async function fetchData() {
+  try {
+    const response = await fetch(url);
+    console.log("response ", response);
+
+    const jsonObj = await response.json();
+    console.log("jsonObj", jsonObj);
+    if (!response.ok) {
+      console.log("Bad request ", response);
+    } else {
+      console.log("Successfull response ", response);
+    }
+  } catch (error) {
+    console.log("Catch block", error);
+  }
+}
 
 fetchData();
