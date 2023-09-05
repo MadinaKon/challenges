9f2b325e0b8a478deb8fd64d54d50bad669811fa
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2023-09-05 12:08:24 +0200

    first methods and tests implemented

--- js/js-unit-testing/js-unit-testing_people/index.js
+++ js/js-unit-testing/js-unit-testing_people/index.js
@@ -2,11 +2,17 @@ export function getFirstNames(people) {
   return people.map((person) => person.firstName);
 }
 
-export function getFullNames(people) {}
+export function getFullNames(people) {
+  return people.map((person) => `${person.firstName} ${person.lastName}`);
+}
 
-export function getNameAndAge(people) {}
+export function getNameAndAge(people) {
+  return people.map((person) => `${person.lastName} (${person.age})`);
+}
 
-export function getPeopleByAge(people, age) {}
+export function getPeopleByAge(people, age) {
+  
+}
 
 export function getPeopleNamesOlderThan(people, age) {}
 
