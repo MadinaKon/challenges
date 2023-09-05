f63577cc52356ff209f2884988f85a49214cb661
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2023-09-05 11:04:05 +0200

    solving unit tests for getAnimal function

--- js/js-unit-testing/js-unit-testing_get-animal/index.js
+++ js/js-unit-testing/js-unit-testing_get-animal/index.js
@@ -1,1 +1,9 @@
-export function getAnimal() {}
+export function getAnimal(animal) {
+  if (animal === "cats") {
+    return `I totally love cats!`;
+  }
+  if (typeof animal === "string") {
+    return `I like ${animal}!`;
+  }
+  return `I do not like animals at all!`;
+}
