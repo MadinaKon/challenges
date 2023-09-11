79334ba39e2b8ab9e6108ad8aa00e0508c12448f
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2023-09-11 16:32:30 +0200

    mood are added

--- react/react-props/react-props_smiley/src/App.js
+++ react/react-props/react-props_smiley/src/App.js
@@ -2,5 +2,14 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return (
+    <h1>
+      <Smiley isHappy={true} />
+      <Smiley isHappy={false} />
+    </h1>
+  );
+}
+
+function Smiley({ isHappy }) {
+  return <h2>Current mood:{isHappy ? "🤣" : "🥲"}</h2>;
 }
