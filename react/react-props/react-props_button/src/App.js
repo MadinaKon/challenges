db731818f3b551e86c528c3e555f615f95d8ff45
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2023-09-11 16:53:25 +0200

    three Button components are added

--- react/react-props/react-props_button/src/App.js
+++ react/react-props/react-props_button/src/App.js
@@ -2,5 +2,23 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return (
+    <>
+      <Button color="green" disabled={false} text="click me" />
+      <Button color="red" disabled={false} text="warn me" />
+      <Button color="pink" disabled={true} text="neutral button" />
+    </>
+  );
+}
+
+function Button({ color, disabled, text }) {
+  return (
+    <button
+      type="button"
+      disabled={disabled}
+      style={{ height: "50px", backgroundColor: "white", color: color }}
+    >
+      {text}
+    </button>
+  );
 }
