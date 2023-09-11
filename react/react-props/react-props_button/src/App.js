72fdf07b6fa1b2d8bd7cdc67ec34e0334dc47b29
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2023-09-11 21:15:13 +0200

    add handleClick

--- react/react-props/react-props_button/src/App.js
+++ react/react-props/react-props_button/src/App.js
@@ -2,33 +2,40 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
+  function handleClick() {
+    console.log("button component");
+  }
   return (
     <>
       <Button
         color="green"
         disabled={false}
         text="click me"
-        onButtonClick={() => console.log("click green")}
+        onClick={handleClick}
       />
       <Button
         color="red"
         disabled={false}
         text="warn me"
-        onButtonClick={() => console.log("click red")}
+        onClick={handleClick}
       />
-      <Button color="pink" disabled={true} text="neutral button" />
+      <Button
+        color="pink"
+        disabled={true}
+        text="neutral button"
+        onClick={handleClick}
+      />
     </>
   );
 }
 
-function Button({ color = "blue", disabled, text, onButtonClick }) {
+function Button({ color = "blue", disabled, text, onClick }) {
   return (
     <button
       type="button"
       disabled={disabled}
       style={{ height: "50px", backgroundColor: "white", color: color }}
-      // onClick={onButtonClick}
-      onClick={() => alert("you cliked the button")}
+      onClick={onClick}
     >
       {text}
     </button>
