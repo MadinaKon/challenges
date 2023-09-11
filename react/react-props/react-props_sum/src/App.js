47939917a1aa4e854fa717f2bf76ec8a80751fac
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2023-09-11 21:25:41 +0200

    pass props

--- react/react-props/react-props_sum/src/App.js
+++ react/react-props/react-props_sum/src/App.js
@@ -1,15 +1,7 @@
 import React from "react";
 import "./styles.css";
-
-function Sum({ valueA, valueB }) {
-  const sum = valueA + valueB;
-  return (
-    <h3>
-      The sum of {valueA} + {valueB} equals to {sum}{" "}
-    </h3>
-  );
-}
+import { Sum } from "./Sum";
 
 export default function App() {
-  return <Sum valueA={99} valueB={33} />;
+  return <Sum valueA={100} valueB={33} />;
 }
