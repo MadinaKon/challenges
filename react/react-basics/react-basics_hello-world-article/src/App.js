5ab68f16c594a720da3bd3f150392750d6731d16
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2023-09-11 11:05:48 +0200

    add component HelloWorldArticle

--- react/react-basics/react-basics_hello-world-article/src/App.js
+++ react/react-basics/react-basics_hello-world-article/src/App.js
@@ -1,6 +1,19 @@
 import React from "react";
 import "./styles.css";
 
+function HelloWorldArticle() {
+  return (
+    <article>
+      <h1>I am a heading inside a component</h1>
+      <p>I am a paragraph inside a component</p>
+    </article>
+  );
+}
+
 export default function App() {
-  return <div>Replace me with your Component!</div>;
+  return (
+    <div>
+      <HelloWorldArticle />
+    </div>
+  );
 }
