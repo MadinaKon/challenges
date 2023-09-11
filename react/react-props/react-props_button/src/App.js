043f217b3964aa3ac2d483dcf52206f213f9a198
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2023-09-11 17:55:08 +0200

    add a Click Event

--- react/react-props/react-props_button/src/App.js
+++ react/react-props/react-props_button/src/App.js
@@ -4,19 +4,31 @@ import "./styles.css";
 export default function App() {
   return (
     <>
-      <Button color="green" disabled={false} text="click me" />
-      <Button color="red" disabled={false} text="warn me" />
+      <Button
+        color="green"
+        disabled={false}
+        text="click me"
+        onButtonClick={() => console.log("click green")}
+      />
+      <Button
+        color="red"
+        disabled={false}
+        text="warn me"
+        onButtonClick={() => console.log("click red")}
+      />
       <Button color="pink" disabled={true} text="neutral button" />
     </>
   );
 }
 
-function Button({ color, disabled, text }) {
+function Button({ color = "blue", disabled, text, onButtonClick }) {
   return (
     <button
       type="button"
       disabled={disabled}
       style={{ height: "50px", backgroundColor: "white", color: color }}
+      // onClick={onButtonClick}
+      onClick={() => alert("you cliked the button")}
     >
       {text}
     </button>
