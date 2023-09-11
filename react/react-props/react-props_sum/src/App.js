84822550e4cba49a84f1eb8fc51608d90480bac5
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2023-09-11 16:12:45 +0200

    sum done

--- react/react-props/react-props_sum/src/App.js
+++ react/react-props/react-props_sum/src/App.js
@@ -1,6 +1,18 @@
 import React from "react";
 import "./styles.css";
 
+function Sum({ valueA, valueB }) {
+  return (
+    <h3>
+      The sum of {valueA} + {valueB} equals to {valueA + valueB}{" "}
+    </h3>
+  );
+}
+
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return (
+    <h1>
+      <Sum valueA={99} valueB={33} />
+    </h1>
+  );
 }
