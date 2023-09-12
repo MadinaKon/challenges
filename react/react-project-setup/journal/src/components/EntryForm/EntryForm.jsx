5e71705449e6d42980919ead3767e0c5e52e071e
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2023-09-12 21:52:17 +0200

    refactor EntryForm component to use BEM

--- react/react-project-setup/journal/src/components/EntryForm/EntryForm.jsx
+++ react/react-project-setup/journal/src/components/EntryForm/EntryForm.jsx
@@ -2,17 +2,30 @@ import "./EntryForm.css";
 
 export default function EntryForm() {
   return (
-    <form>
-      <title>New Entry</title>
-      <label htmlFor="name">Motto</label>
-      <input type="text" name="name" />
-      <hr />
-      <label htmlFor="name">
-        Notes <textarea name="notes">Notes...</textarea>
-      </label>
-      <button type="submit" className="form-create-button">
-        Create
-      </button>
-    </form>
+    <>
+      <form className="form">
+        <div className="form__group">
+          <label className="form__label" htmlFor="motto">
+            Motto:
+          </label>
+          <input className="form__input" type="text" id="motto" name="motto" />
+        </div>
+        <div className="form__group">
+          <label className="form__label" htmlFor="notes">
+            Notes:
+          </label>
+          <textarea
+            className="form__input form__input--textarea"
+            id="notes"
+            name="notes"
+          ></textarea>
+        </div>
+        <div className="form__group">
+          <button className="form__button" type="submit">
+            Create
+          </button>
+        </div>
+      </form>
+    </>
   );
 }
