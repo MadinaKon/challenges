9549f735d3cb879c387932740fa3ee2279ee81d7
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2023-09-12 11:34:02 +0200

    add Link component

--- react/react-nesting/react-nesting_navigation/src/App.js
+++ react/react-nesting/react-nesting_navigation/src/App.js
@@ -4,24 +4,24 @@ import avatar from "./img/avatar.jpg";
 import logo from "./img/logo.jpg";
 import Header from "./components/Header";
 import Navigation from "./components/Navigation";
-
+import Link from "./components/Link";
 export default function App() {
   return (
     <>
       <Header className="header">
-        <a href="#">
+        <Link href="#">
           <img className="round-image" src={logo} alt="logo" />
-        </a>
+        </Link>
         <Navigation>
-          <a className="navigation__link" href="#home">
+          <Link className="navigation__link" href="#home">
             Home
-          </a>
-          <a className="navigation__link" href="#about">
+          </Link>
+          <Link className="navigation__link" href="#about">
             About
-          </a>
-          <a className="navigation__link" href="#impressum">
+          </Link>
+          <Link className="navigation__link" href="#impressum">
             Impressum
-          </a>
+          </Link>
         </Navigation>
         <button
           type="button"
