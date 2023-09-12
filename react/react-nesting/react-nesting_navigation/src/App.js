9c8df91983fffb2d4326a0048a48ee22b6f4dd61
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2023-09-12 11:28:54 +0200

    add Navigation component

--- react/react-nesting/react-nesting_navigation/src/App.js
+++ react/react-nesting/react-nesting_navigation/src/App.js
@@ -3,6 +3,7 @@ import "./styles.css";
 import avatar from "./img/avatar.jpg";
 import logo from "./img/logo.jpg";
 import Header from "./components/Header";
+import Navigation from "./components/Navigation";
 
 export default function App() {
   return (
@@ -11,7 +12,7 @@ export default function App() {
         <a href="#">
           <img className="round-image" src={logo} alt="logo" />
         </a>
-        <nav>
+        <Navigation>
           <a className="navigation__link" href="#home">
             Home
           </a>
@@ -21,7 +22,7 @@ export default function App() {
           <a className="navigation__link" href="#impressum">
             Impressum
           </a>
-        </nav>
+        </Navigation>
         <button
           type="button"
           onClick={() => console.log("I could toggle a profile!")}
