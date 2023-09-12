855e5a1bb03643a5b059fed901d5271208f57de9
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2023-09-12 22:57:26 +0200

    remove badge  position: absolute

--- react/react-project-setup/journal/src/components/Tabs/Tabs.jsx
+++ react/react-project-setup/journal/src/components/Tabs/Tabs.jsx
@@ -3,15 +3,7 @@ import "./Tabs.css";
 export default function Tabs({ text, count }) {
   return (
     <>
-      {/* <ul className="tabs__list">
-        <li className="tabs__item tabs__item--active">
-          <a href="#tab1" className="tabs__link">
-            {" "}
-            {text} {count}
-          </a>
-        </li>
-      </ul> */}
-      <button type="button" style={{ border: "solid black 1px" }}>
+      <button type="button">
         {text}
         {count}
       </button>
