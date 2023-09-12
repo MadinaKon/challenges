b267cb3cb741677ec3c6afe1ec7a45d48cbc704a
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2023-09-12 22:22:41 +0200

    Tabs are buttons

--- react/react-project-setup/journal/src/components/Tabs/Tabs.jsx
+++ react/react-project-setup/journal/src/components/Tabs/Tabs.jsx
@@ -3,14 +3,18 @@ import "./Tabs.css";
 export default function Tabs({ text, count }) {
   return (
     <>
-      <ul className="tabs__list">
+      {/* <ul className="tabs__list">
         <li className="tabs__item tabs__item--active">
           <a href="#tab1" className="tabs__link">
             {" "}
             {text} {count}
           </a>
         </li>
-      </ul>
+      </ul> */}
+      <button type="button" style={{ border: "solid black 1px" }}>
+        {text}
+        {count}
+      </button>
     </>
   );
 }
