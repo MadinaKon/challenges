427815d5aca664dadc9fbbbc686eca1c2ed3dc64
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2023-09-12 22:14:30 +0200

    list to display Tabs

--- react/react-project-setup/journal/src/components/Tabs/Tabs.jsx
+++ react/react-project-setup/journal/src/components/Tabs/Tabs.jsx
@@ -1,19 +1,16 @@
-// export default function Tabs({ text, count }) {
-//   return (
-//     <>
-//       <div style={{ border: "solid black 1px" }}>
-//         {text} {count}
-//       </div>
-//     </>
-//   );
-// }
+import "./Tabs.css";
 
 export default function Tabs({ text, count }) {
   return (
     <>
-      <button type="button" style={{ border: "solid black 1px" }}>
-        {text} {count}
-      </button>
+      <ul className="tabs__list">
+        <li className="tabs__item tabs__item--active">
+          <a href="#tab1" className="tabs__link">
+            {" "}
+            {text} {count}
+          </a>
+        </li>
+      </ul>
     </>
   );
 }
