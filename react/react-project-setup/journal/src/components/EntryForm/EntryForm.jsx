e3c591b996c3dab32a1d953260c99e8da89e61bc
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2023-09-12 20:45:33 +0200

    refactoring components

--- react/react-project-setup/journal/src/components/EntryForm/EntryForm.jsx
+++ react/react-project-setup/journal/src/components/EntryForm/EntryForm.jsx
@@ -1,13 +1,17 @@
+import "./EntryForm.css";
+
 export default function EntryForm() {
   return (
     <form>
+      <title>New Entry</title>
       <label htmlFor="name">
         Motto <input type="text" name="name" />
       </label>
+      <hr />
       <label htmlFor="name">
         Notes <textarea name="notes">Notes...</textarea>
       </label>
-      <button type="submit" style={{ backgroundColor: "orange" }}>
+      <button type="submit" className="form-create-button">
         Create
       </button>
     </form>
