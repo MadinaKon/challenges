69f626e7c8f52b1b1018eaf9d0d170eb2fdf4c8e
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2023-09-13 15:50:37 +0200

    Tag compoent refactor

--- react/react-with-arrays/react-with-arrays_users/src/components/Card.js
+++ react/react-with-arrays/react-with-arrays_users/src/components/Card.js
@@ -1,12 +1,17 @@
 import "./Card.css";
 import Tag from "./Tag";
 import Button from "./Button";
+
 function Card({ user }) {
+  console.log("USER ", user);
   return (
     <article className="card">
       <h2>{user.name}</h2>
       <ul className="card__taglist">
-        <Tag tag={user.roles[0]} />
+        {/* <Tag tag={user.roles[0]} /> */}
+        {user?.roles.map((roles) => (
+          <Tag tag={roles} key={roles.id} />
+        ))}
       </ul>
       <p>{user.about}</p>
       <Button>edit</Button>
