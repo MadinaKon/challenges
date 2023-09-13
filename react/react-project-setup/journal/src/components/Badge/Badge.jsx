5309dac5a037647a1599104df42485fbc353deda
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2023-09-13 13:30:13 +0200

    add SVG as ReactComponent and use in the Badge

--- react/react-project-setup/journal/src/components/Badge/Badge.jsx
+++ react/react-project-setup/journal/src/components/Badge/Badge.jsx
@@ -1,9 +1,19 @@
-import "./Badge.css";
+import { ReactComponent as StarFilled } from "./star-filled.svg";
+import { ReactComponent as Star } from "./star.svg";
+
+import { useState } from "react";
 
 export default function Badge() {
+  const [isFavorite, setIsFavorite] = useState(false);
   return (
-    <button type="button" className="badge">
-      addToFavorite
+    <button
+      className="favorite-button"
+      onClick={() => {
+        setIsFavorite(!isFavorite);
+      }}
+      aria-label="favorite"
+    >
+      {isFavorite ? <StarFilled /> : <Star />}
     </button>
   );
 }
