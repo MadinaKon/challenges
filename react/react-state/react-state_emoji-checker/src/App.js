13e20b787e8f63ea2a334801bff932938c79c971
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2023-09-13 13:01:06 +0200

    add possible solutions

--- react/react-state/react-state_emoji-checker/src/App.js
+++ react/react-state/react-state_emoji-checker/src/App.js
@@ -1,14 +1,57 @@
 import React, { useState } from "react";
 import "./styles.css";
 
+// export default function App() {
+//   const [code, setCode] = useState("");
+//   // expected sequence
+//   const validCode = "🐡🐠🐋";
+
+//   const handleClick = (event) => {
+//     const value = event.target.textContent;
+//     setCode(code + value);
+//   };
+
+//   const handleReset = () => {
+//     setCode("");
+//   };
+
+//   return (
+//     <div className="container">
+//       <div className="button-container">
+//         <button type="button" onClick={handleClick}>
+//           <span role="img" aria-label="Pufferfish">
+//             🐡
+//           </span>
+//         </button>
+//         <button type="button" onClick={handleClick}>
+//           <span role="img" aria-label="Whale">
+//             🐋
+//           </span>
+//         </button>
+//         <button type="button" onClick={handleClick}>
+//           <span role="img" aria-label="Clownfish">
+//             🐠
+//           </span>
+//         </button>{" "}
+//       </div>
+
+//       <button type="button" onClick={handleReset}>
+//         Reset
+//       </button>
+//       <h2>{code.length ? code : "?"}</h2>
+
+//       {code === validCode && <p>Valid code!</p>}
+//     </div>
+//   );
+// }
+
 export default function App() {
   const [code, setCode] = useState("");
   // expected sequence
   const validCode = "🐡🐠🐋";
 
-  const handleClick = (event) => {
-    const value = event.target.textContent;
-    setCode(code + value);
+  const handleClick = (emoji) => {
+    setCode(code + emoji);
   };
 
   const handleReset = () => {
@@ -18,17 +61,17 @@ export default function App() {
   return (
     <div className="container">
       <div className="button-container">
-        <button type="button" onClick={handleClick}>
+        <button type="button" onClick={() => handleClick("🐡")}>
           <span role="img" aria-label="Pufferfish">
             🐡
           </span>
         </button>
-        <button type="button" onClick={handleClick}>
+        <button type="button" onClick={() => handleClick("🐋")}>
           <span role="img" aria-label="Whale">
             🐋
           </span>
         </button>
-        <button type="button" onClick={handleClick}>
+        <button type="button" onClick={() => handleClick("🐠")}>
           <span role="img" aria-label="Clownfish">
             🐠
           </span>
