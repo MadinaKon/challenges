cb16b3ccfb60dc00a5c973d4280c6b616007efd7
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2023-09-14 11:56:56 +0200

    Pass your handle functions to the Counter component

--- react/react-state-2/react-state-2_table-reservation/src/App.js
+++ react/react-state-2/react-state-2_table-reservation/src/App.js
@@ -1,13 +1,28 @@
 import React from "react";
 import Counter from "./components/Counter";
 import "./styles.css";
+import { useState } from "react";
 
 export default function App() {
+  const [people, setPeople] = useState(0);
+
+  function handleIncrement() {
+    setPeople(people + 1);
+  }
+
+  function handleDecrement() {
+    setPeople(people - 1);
+  }
+
   return (
     <div className="container">
       <h1>Place a Table Reservation</h1>
-      <Counter />
-      <p>You are going to reserve a table for 2 people.</p>
+      <Counter onIncrement={handleIncrement} onDecrement={handleDecrement} />
+      {people > 0 ? (
+        <p>You are going to reserve a table for {people} people.</p>
+      ) : (
+        <p>{people}</p>
+      )}
     </div>
   );
 }
