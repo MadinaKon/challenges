1d6048690bad8a3d06b35418f447c424603909cd
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2023-09-14 11:21:16 +0200

    add functions

--- react/react-state-2/react-state-2_colored-number/src/components/ColoredNumber.js
+++ react/react-state-2/react-state-2_colored-number/src/components/ColoredNumber.js
@@ -1,4 +1,5 @@
 export default function ColoredNumber({ value }) {
+  console.log("value ", value);
   return (
     <h2 style={{ color: `hsl(${230 - (value / 100) * 230}deg 100% 50%)` }}>
       {value}
--- react/react-state-2/react-state-2_colored-number/src/components/Counter.js
+++ react/react-state-2/react-state-2_colored-number/src/components/Counter.js
@@ -1,15 +1,26 @@
+import { useState } from "react";
 import ColoredNumber from "./ColoredNumber";
 
 export default function Counter() {
+  const [count, setCount] = useState(0);
+
+  function handleIncrement() {
+    setCount(count + 1);
+  }
+  function handleDecrement() {
+    setCount(count - 1);
+  }
+
   return (
     <div className="counter">
       <h1>Colored Number</h1>
-      <ColoredNumber value={0} />
+      <ColoredNumber value={count} />
       <div className="counter__buttons-container">
         <button
           type="button"
           className="counter__button"
           aria-label="increment count"
+          onClick={handleIncrement}
         >
           +
         </button>
@@ -17,6 +28,7 @@ export default function Counter() {
           type="button"
           className="counter__button"
           aria-label="decrement count"
+          onClick={handleDecrement}
         >
           -
         </button>
