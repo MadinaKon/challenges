b05e38df8975e93cb283434003bac03ca61e810f
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2023-09-15 16:19:00 +0200

    The “Next Page” button done

--- react/react-effects-and-fetch/react-effects-and-fetch_pokemon-api-with-paging/src/components/PokemonList.js
+++ react/react-effects-and-fetch/react-effects-and-fetch_pokemon-api-with-paging/src/components/PokemonList.js
@@ -2,12 +2,13 @@ import { useEffect, useState } from "react";
 
 export default function PokemonList() {
   const [pokemon, setPokemon] = useState([]);
+  const [currentPage, setPage] = useState(0);
 
   useEffect(() => {
     async function loadPokemon() {
       try {
         const response = await fetch(
-          "https://pokeapi.co/api/v2/pokemon?offset=0"
+          `https://pokeapi.co/api/v2/pokemon?offset=${currentPage}`
         );
         const data = await response.json();
         setPokemon(data.results);
@@ -17,12 +18,15 @@ export default function PokemonList() {
     }
 
     loadPokemon();
-  }, []);
+  }, [currentPage]);
 
   return (
     <main>
       <button type="button">Previous Page</button>
-      <button type="button">Next Page</button>
+      <button type="button" onClick={() => setPage(currentPage + 20)}>
+        Next Page
+      </button>
+
       <ul>
         {pokemon.map(({ name }) => (
           <li key={name}>{name}</li>
