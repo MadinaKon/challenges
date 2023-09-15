581ac248ff66c6ee93d41327310c9869e3c81ab0
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2023-09-15 11:19:31 +0200

    lift state to App and Pass the entries state via the entries prop down to the EntriesSection

--- react/react-state-3/react-state-3_journal-app-form/src/components/EntriesSection/index.js
+++ react/react-state-3/react-state-3_journal-app-form/src/components/EntriesSection/index.js
@@ -6,37 +6,7 @@ import Tab from "../Tab";
 import Badge from "../Badge";
 import { Fragment } from "react";
 
-const entries = [
-  {
-    id: 1000,
-    date: "Feb 5, 2025",
-    motto: "We are in a state of chaos",
-    notes:
-      "Today I learned about React State. It was fun! I can't wait to learn more.",
-  },
-  {
-    id: 999,
-    date: "Feb 4, 2025",
-    motto: "Props, Props, Props",
-    notes:
-      "Today I learned about React Props. Mad props to everyone who understands this!",
-  },
-  {
-    id: 998,
-    date: "Feb 3, 2025",
-    motto: "How to nest components online fast",
-    notes:
-      "Today I learned about React Components and how to nest them like a pro. Application design is so much fun!",
-  },
-  {
-    id: 997,
-    date: "Feb 2, 2025",
-    motto: "I'm a React Developer",
-    notes: "My React-ion when I learned about React: Yay!",
-  },
-];
-
-export default function EntriesSection() {
+export default function EntriesSection({ entries }) {
   return (
     <section className="entries-section">
       <Tabs>
