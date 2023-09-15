7b83fc62b2e1d4c23db476c70aab8fc1a5d63991
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2023-09-15 12:15:52 +0200

    adjust the state handling

--- react/react-state-3/react-state-3_journal-app-favorite-button-shared-state/src/App.js
+++ react/react-state-3/react-state-3_journal-app-favorite-button-shared-state/src/App.js
@@ -53,12 +53,23 @@ function App() {
     ]);
   }
 
+  function handleToggleFavorite(id) {
+    setEntries(
+      entries.map((entry) =>
+        entry.id === id ? { ...entry, isFavorite: !entry.isFavorite } : entry
+      )
+    );
+  }
+
   return (
     <div className="app">
       <Header />
       <main className="app__main">
         <EntryForm onAddEntry={handleAddEntry} />
-        <EntriesSection entries={entries} />
+        <EntriesSection
+          entries={entries}
+          onToggleFavorite={handleToggleFavorite}
+        />
       </main>
       <Footer />
     </div>
