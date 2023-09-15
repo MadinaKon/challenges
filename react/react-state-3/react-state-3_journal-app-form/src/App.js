87761c2e74db3b0dab66699bc6bec05a615857a1
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2023-09-15 11:38:04 +0200

    add a new object

--- react/react-state-3/react-state-3_journal-app-form/src/App.js
+++ react/react-state-3/react-state-3_journal-app-form/src/App.js
@@ -4,6 +4,7 @@ import EntryForm from "./components/EntryForm";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import { useState } from "react";
+import { uid } from "uid";
 
 const initialEntries = [
   {
@@ -37,11 +38,20 @@ const initialEntries = [
 
 function App() {
   const [entries, setEntries] = useState(initialEntries);
+
+  function handleAddEntry(newEntry) {
+    const date = new Date().toLocaleDateString("en-us", {
+      dateStyle: "medium",
+    });
+    const id = uid();
+
+    setEntries([{ ...newEntry, date: date, id: id }, ...entries]);
+  }
   return (
     <div className="app">
       <Header />
       <main className="app__main">
-        <EntryForm />
+        <EntryForm onAddEntry={handleAddEntry} />
         <EntriesSection entries={entries} />
       </main>
       <Footer />
