f9f51eca01bde67f6949f8528c14296219d3e48c
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2023-09-16 20:54:49 +0200

    display a ‘⏳’ icon while waiting for the response

--- react/react-effects-and-fetch/react-effects-and-fetch_api-status/src/components/StatusCheck/index.js
+++ react/react-effects-and-fetch/react-effects-and-fetch_api-status/src/components/StatusCheck/index.js
@@ -1,33 +1,42 @@
+import { useState } from "react";
 import "./StatusCheck.css";
 
 const apiStatusUrl = "https://example-apis.vercel.app/api/status";
 
 export default function StatusCheck() {
-  const statusIcon = "⁉️";
-  // Something needs to change here…
-  // ↙️
-  function handleCheckApiStatus() {
-    /**
-     * Hint 1:
-     * Use the `fetch()` function and pass the `apiStatusUrl` into it
-     *
-     * Hint 2:
-     * The fetch function returns a promise which resolves to a Response
-     * object once it is ready.
-     *
-     * Hint 3:
-     * The Response object has a `ok` property which is true if the response
-     * is okay and false if it is not.
-     **/
-    // --v-- write your code here --v--
-    // --^-- write your code here --^--
+  let [statusIcon, setStatusIcon] = useState("⁉️");
+  const [isLoading, setIsLoading] = useState(false);
+
+  async function handleCheckApiStatus() {
+    setIsLoading(true);
+    try {
+      const response = await fetch(apiStatusUrl);
+
+      console.log("response ", response);
+      console.log("response.ok ", response.ok);
+
+      if (response.ok === true) {
+        setStatusIcon("✅");
+        setIsLoading(false);
+      } else {
+        setStatusIcon("❌");
+        setIsLoading(false);
+      }
+    } catch (e) {
+      console.log(e);
+    }
   }
 
   return (
     <article className="status-check">
       <div className="status-check__wrapper">
         <h2 className="status-check__heading">Status:</h2>
-        <span className="status-check__icon">{statusIcon}</span>
+        {isLoading ? (
+          "⏳"
+        ) : (
+          <span className="status-check__icon">{statusIcon}</span>
+        )}
+        {/* <span className="status-check__icon">{statusIcon}</span> */}
       </div>
       <button
         type="button"
