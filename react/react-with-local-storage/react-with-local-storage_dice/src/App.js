0af575034ca5578d22e8597266a6f7b5391d805c
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2023-09-18 12:25:34 +0200

    lifted state up from D6Button component  to App and passed props

--- react/react-with-local-storage/react-with-local-storage_dice/src/App.js
+++ react/react-with-local-storage/react-with-local-storage_dice/src/App.js
@@ -1,15 +1,25 @@
 import { D6Button } from "./components/D6Button";
 import History from "./components/History";
 import "./styles.css";
+import { useState } from "react";
+import { getD6Roll } from "./utils";
 
 export default function App() {
+  const [rolls, setRolls] = useState([]);
+
+  const handleRoll = () => {
+    setRolls([{ value: getD6Roll(), time: Date.now() }, ...rolls]);
+  };
+
+  const currentRollValue = rolls[0]?.value;
+
   return (
     <div className="app">
       <main className="app__main">
-        <D6Button />
+        <D6Button value={currentRollValue} onRoll={handleRoll} />
       </main>
       <aside className="app__aside">
-        <History rolls={[]} />
+        <History rolls={rolls} />
       </aside>
     </div>
   );
