ec2d12bf74110fbe1191afe3026ab6f738032837
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2023-09-28 12:26:51 +0200

    add immer and use the draft object and dot-notation to reach the keys with the new value

--- react/react-immutable-state/react-immutable-state_form/components/Form/index.js
+++ react/react-immutable-state/react-immutable-state_form/components/Form/index.js
@@ -1,8 +1,46 @@
 import { useState } from "react";
 import { StyledForm, StyledInputContainer } from "./Form.styled";
+import { useImmer } from "use-immer";
 
 export default function Form() {
-  const [mountain, setMountain] = useState({
+  // const [mountain, setMountain] = useState({
+  //   name: "Mount Everest",
+  //   values: {
+  //     altitude: 8848,
+  //     mountainRange: "Himalayas",
+  //   },
+  // });
+
+  // function handleNameChange(event) {
+  //   setMountain({
+  //     ...mountain,
+  //     name: event.target.value,
+  //   });
+  // }
+
+  // function handleAltitudeChange(event) {
+  //   setMountain({
+  //     ...mountain,
+  //     values: {
+  //       ...mountain.values,
+  //       altitude: event.target.value,
+  //     },
+  //   });
+  //   //  setMountain({ ...mountain.values, altitude: event.target.value });
+  // }
+
+  // function handleMountainRangeChange(event) {
+  //   setMountain({
+  //     ...mountain,
+  //     values: {
+  //       ...mountain.values,
+  //       mountainRange: event.target.value,
+  //     },
+  //   });
+  //   //  setMountain({ ...mountain.values, mountainRange: event.target.value });
+  // }
+
+  const [mountain, updateMountain] = useImmer({
     name: "Mount Everest",
     values: {
       altitude: 8848,
@@ -11,32 +49,21 @@ export default function Form() {
   });
 
   function handleNameChange(event) {
-    setMountain({
-      ...mountain,
-      name: event.target.value,
+    updateMountain((draft) => {
+      draft.name = event.target.value;
     });
   }
 
   function handleAltitudeChange(event) {
-    setMountain({
-      ...mountain,
-      values: {
-        ...mountain.values,
-        altitude: event.target.value,
-      },
+    updateMountain((draft) => {
+      draft.values.altitude = event.target.value;
     });
-    //  setMountain({ ...mountain.values, altitude: event.target.value });
   }
 
   function handleMountainRangeChange(event) {
-    setMountain({
-      ...mountain,
-      values: {
-        ...mountain.values,
-        mountainRange: event.target.value,
-      },
+    updateMountain((draft) => {
+      draft.values.mountainRange = event.target.value;
     });
-    //  setMountain({ ...mountain.values, mountainRange: event.target.value });
   }
 
   return (
