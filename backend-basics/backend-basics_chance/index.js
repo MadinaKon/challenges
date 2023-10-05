3936c8bab05c6c8bff310b3c07423aebf6ba0cc2
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2023-10-05 13:50:37 +0200

    use Chance package and make the server respond with the string

--- backend-basics/backend-basics_chance/index.js
+++ backend-basics/backend-basics_chance/index.js
@@ -1,3 +1,3 @@
-// import { server } from "./server.js";
+import { server } from "./server.js";
 
-// server.listen(…
+server.listen(8000);
--- backend-basics/backend-basics_chance/server.js
+++ backend-basics/backend-basics_chance/server.js
@@ -1,3 +1,13 @@
 import { createServer } from "node:http";
+import Chance from "chance";
 
-// export const server = …
+const chance = new Chance();
+const name = chance.name();
+const age = chance.age();
+const profession = chance.profession();
+
+export const server = createServer((request, response) => {
+  response.end(
+    `Hello, my name is ${name} and I am ${age} years old. I am a ${profession}`
+  );
+});
