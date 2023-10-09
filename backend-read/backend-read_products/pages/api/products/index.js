623ddfebccde4a3d33761cc5a28054180c0ea9b6
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2023-10-09 12:20:57 +0200

    refactor handler with async await connection to DB and use the Product model to find all products

--- backend-read/backend-read_products/pages/api/products/index.js
+++ backend-read/backend-read_products/pages/api/products/index.js
@@ -1,5 +1,10 @@
-import { products } from "../../../lib/products";
+import dbConnect from "@/db/connect";
+import Product from "@/db/models/Product";
 
-export default function handler(request, response) {
-  return response.status(200).json(products);
+export default async function handler(request, response) {
+  await dbConnect();
+  if (request.method === "GET") {
+    const products = await Product.find();
+    return response.status(200).json(products);
+  }
 }
